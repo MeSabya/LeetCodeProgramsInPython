d3933580346fed38dc4c22eca2f3f0138f18cfd7
sabyasachi nayak <33947539+MeSabya@users.noreply.github.com>
2023-01-25 19:13:56 +0530

    Update D. Smallest Number Range.py

--- KWayMerge/D. Smallest Number Range.py
+++ KWayMerge/D. Smallest Number Range.py
@@ -1,7 +1,10 @@
 '''
-We can start by inserting the first number from all the arrays in a min-heap. We will keep track of the largest number that we have inserted in the heap (let’s call it currentMaxNumber).
+We can start by inserting the first number from all the arrays in a min-heap.
+We will keep track of the largest number that we have inserted in the heap (let’s call it currentMaxNumber).
 
-In a loop, we’ll take the smallest (top) element from the min-heap andcurrentMaxNumber has the largest element that we inserted in the heap. If these two numbers give us a smaller range, we’ll update our range. Finally, if the array of the top element has more elements, we’ll insert the next element to the heap.
+In a loop, we’ll take the smallest (top) element from the min-heap andcurrentMaxNumber has the largest element that we inserted in the heap.
+If these two numbers give us a smaller range, we’ll update our range. 
+Finally, if the array of the top element has more elements, we’ll insert the next element to the heap.
 
 We can finish searching the minimum range as soon as an array is completed or, in other terms, the heap has less than ‘M’ elements.
 '''
